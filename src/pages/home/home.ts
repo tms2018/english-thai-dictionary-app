2fb3b7e5b64bc727abcd663f2d7111bf302ea0dd
Tim Schmidt <timothymschmidt2018@gmail.com>
2018-10-12 17:43:35 -0400

    Make Home page template display properly structured word data

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,62 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
+import { DatabaseProvider, Word } from "../../providers/database/database";
+import { Observable } from "rxjs";
+import "rxjs/add/operator/skipWhile";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
+  words: Word[] = [
+    {
+      word: "test",
+      pos: "test",
+      definitions: [
+        {
+          definition: "test",
+          translation: "el testo",
+          example: "test"
+        }
+      ]
+    },
+    {
+      word: "test",
+      pos: "test",
+      definitions: [
+        {
+          definition: "test",
+          translation: "el testo",
+          example: "test"
+        }
+      ]
+    },
+    {
+      word: "test",
+      pos: "test",
+      definitions: [
+        {
+          definition: "test",
+          translation: "el testo",
+          example: "test"
+        }
+      ]
+    }
+  ];
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public db: DatabaseProvider) {
+    this.find("A");
   }
 
+  find(word) {
+    this.db
+      .ready()
+      .skipWhile(val => !val)
+      .subscribe(_ => {
+        // Observable.fromPromise(this.db.fts(word)).subscribe(
+        //   matches => (this.words = matches)
+        // );
+      });
+  }
 }
