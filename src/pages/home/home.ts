50589d031efd632909284e5441ca326e444908f9
Tim Schmidt <timothymschmidt2018@gmail.com>
2018-10-13 14:29:16 -0400

    Display all search results on their respective pages and add TODOs

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,8 +1,8 @@
+// TODO handle navigation from search page
+
 import { Component } from "@angular/core";
 import { NavController } from "ionic-angular";
 import { DatabaseProvider, Word } from "../../providers/database/database";
-import { Observable } from "rxjs";
-import "rxjs/add/operator/skipWhile";
 
 @Component({
   selector: "page-home",
@@ -12,14 +12,22 @@ export class HomePage {
   words: Word[] = [];
 
   constructor(public navCtrl: NavController, public db: DatabaseProvider) {
-    this.find("A");
+    this.find("Love");
   }
 
   find(word) {
     this.db.fts(word).subscribe(res => {
       res.then(matches => {
-        this.words = matches.filter(val => val !== null);
+        this.words = flattenDeep(matches.filter(val => val !== null));
       });
     });
   }
+}
+
+function flattenDeep(arr1) {
+  return arr1.reduce(
+    (acc, val) =>
+      Array.isArray(val) ? acc.concat(flattenDeep(val)) : acc.concat(val),
+    []
+  );
 }
--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -1,10 +1,37 @@
+// TODO: move flattening of arrays into the database provider
+// TODO: replace placeholder html with message box to input search text
+// TODO: display loading spinner while searching for words
+// TODO: pass search results to the home screen to display
+
 import { Component } from "@angular/core";
 import { NavController } from "ionic-angular";
+import { DatabaseProvider, Word } from "../../providers/database/database";
 
 @Component({
   selector: "page-search",
   templateUrl: "search.html"
 })
 export class SearchPage {
-  constructor(public navCtrl: NavController) {}
+  words: Word[] = [];
+
+  constructor(public navCtrl: NavController, public db: DatabaseProvider) {
+    this.find("I love you");
+  }
+
+  find(text) {
+    const words: String[] = text.split(" ");
+    this.db.findAll(words).subscribe(res => {
+      res.then(matches => {
+        this.words = flattenDeep(matches);
+      });
+    });
+  }
+}
+
+function flattenDeep(arr1) {
+  return arr1.reduce(
+    (acc, val) =>
+      Array.isArray(val) ? acc.concat(flattenDeep(val)) : acc.concat(val),
+    []
+  );
 }
