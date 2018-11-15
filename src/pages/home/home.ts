431e35f7d9140682dc0c559c46f7c41348c373d4
Tim Schmidt <timothymschmidt2018@gmail.com>
2018-11-15 13:19:31 -0500

    Add loading indicators

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,6 +10,7 @@ export class HomePage {
   words: Word[] = [];
   notFound: String[] = [];
   searchType: String = 'fts';
+  isLoading: Boolean = false;
 
   constructor(
     private db: DatabaseProvider,
@@ -23,21 +24,26 @@ export class HomePage {
     word = this.nlp.tokenize(word).join(' ').trim();
     if (word === '') return;
 
+    this.isLoading = true;
     this.db.fts(word).subscribe(res => {
       this.words = res;
       this.notFound = [];
+      this.isLoading = false;
     });
   }
 
   find(text: String) {
-    const words: String[] = this.nlp.tokenize(text);
+    const words: String[] = this.nlp.tokenizeAndStem(text);
     if (!words) return;
 
+    this.isLoading = true;
     this.db.findAll(words).subscribe(results => {
       this.words = <Word[]>results.filter(word => !word.hasOwnProperty('notFound'));
       this.notFound = results
         .filter(word => word.hasOwnProperty('notFound'))
         .map(({ notFound }: WordNotFound) => notFound)
+
+      this.isLoading = false;
     });
 
   }
